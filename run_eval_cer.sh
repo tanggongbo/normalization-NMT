eae7ea4be81a91fde38f5bd34d414302c5d1e480
tanggongbo <tanggongbo@126.com>
2018-06-12 00:19:14 +0200

    Create run_eval_cer.sh

---
+++ run_eval_cer.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# evaluate the predictions of changed and unchanged set 
+
+for LAN in "en" "de" "hu" "ic" "se"
+do
+
+  for mod in "tanh-noatt" "gru-noatt" "lstm-noatt" "tanh-att" "gru-att" "lstm-att" "transformer" "bpe100" "bpe200" "bpe300" "bpe500" "bpe1000" "bpe5000"
+  do
+    echo "===================="
+    echo $LAN----$mod
+    echo "===================="
+    for dir in "test" "dev"
+    do
+      echo "------"+${dir}+"--------"      
+      for tp in "ch" "unch"
+      do
+        echo "------"+${tp}+"--------"
+        for beam in 5
+        do
+          python3 eval_cer_sep.py your-prediction-file reference-file source-file cer-error-output-file
+        done
+      done
+    done
+  done
+
+done
